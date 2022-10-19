2d899e1f13221d79bdbe84bbb2491e8f4062fa18
edunoodt <edunoodt@yahoo.com.ar>
2022-10-19 19:57:50 -0300

    Mega Course UDEMY: pruebas simples

--- practica.py
+++ practica.py
@@ -4,3 +4,10 @@ print(x, y)
 z = x
 z = 20
 print(x, y, z)
+
+notas_alumnos = {'juan': 7, 'maría':8.5, 'ernesto': 8.8}
+print (notas_alumnos)
+suma_notas = sum(notas_alumnos.values())
+print(suma_notas)
+
+print(dir(__builtins__))
