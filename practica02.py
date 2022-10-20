7a1616b1545dd113ee2bb5a54e998025a9cd163f
edunoodt <edunoodt@yahoo.com.ar>
2022-10-20 17:37:58 -0300

    Practica de formateo de strings

---
+++ practica02.py
@@ -0,0 +1,13 @@
+nombre = input ('ingrese su nombre: ')
+apellido = input ('ingrese su apellido: ')
+nacim = input ('Fecha de nacimiento: ')
+
+
+
+print ('Señor %s %s, usted nació el %s' % (nombre, apellido, nacim))
+
+calle = input('Calle: ')
+numero = input('Numero: ')
+ciudad = input ('Ciudad: ')
+
+print (f'Señor {apellido}, usted vive en {calle} nro {numero}, ciudad de {ciudad}')
