5fb86fb5ca5639bdce49e8512ddc5e04e91857e2
edunoodt <edunoodt@yahoo.com.ar>
2022-10-20 07:49:28 -0300

    Agrega diccionario

--- practica.py
+++ practica.py
@@ -4,4 +4,11 @@ print(x, y)
 z = x
 z = 20
 print(x, y, z)
-print(x, y, z)
+
+temperaturas = {'mañana': [15.5, 16.7, 18.3],'tarde': [21.3, 22.4, 25.8]}
+
+print (temperaturas)
+
+temperaturas['noche'] = [19.3, 17.9, 16.5]
+
+print (temperaturas)
