41194c41a5e347e73d338c6bb86cbc59520edb2e
edunoodt <edunoodt@yahoo.com.ar>
2022-10-21 17:13:59 -0300

    Iteracion con reemplazo de string

--- practica03.py
+++ practica03.py
@@ -14,3 +14,61 @@ phone_numbers = {"John": "+37682929928", "Marry": "+423998200919"}
  
 for key, value in phone_numbers.items():
     print(f"{key} has as phone number {value}")
+
+phone_numbers = {"John": "+37682929928", "Marry": "+423998200919"}
+ 
+for valor in phone_numbers.values():
+    print(valor.replace('+','00'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
