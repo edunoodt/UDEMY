9987200f07dedfb68482d9c6036aa96bcac43273
edunoodt <edunoodt@yahoo.com.ar>
2022-11-01 19:59:30 -0300

    Manejo de archivos

--- practica06.py
+++ practica06.py
@@ -2,7 +2,19 @@
 mi_arch = open ('textos/fruits.txt')
 print (mi_arch.read())
 mi_arch.close()
-"""
+
 
 with open('textos/fruits.txt') as mi_arch:
     print(mi_arch.read())
+
+"""
+entrada = ''
+cadena = ''
+while entrada != 'xxx':
+    entrada=input('Ingrese vegetal (xxx para salir): ')
+    if entrada != 'xxx':
+        cadena += entrada + '\n'
+
+with open('textos/vegetales.txt',"w") as mi_arch:
+    mi_arch.write(cadena)
+
